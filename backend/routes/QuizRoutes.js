1a85661b8f5e2027e6bfc2a88eafd46c973fdc5b
Ujjwal-Sabharwal <142159680+Ujjwal-Sabharwal@users.noreply.github.com>
2023-08-21 16:12:50 +0530

    Update QuizRoutes.js

--- backend/routes/QuizRoutes.js
+++ backend/routes/QuizRoutes.js
@@ -4,7 +4,7 @@ import { Genere2 } from "../modules/quizgener/Genere2.js";
 import { Genere3 } from "../modules/quizgener/Genere3.js";
 import { Genere4 } from "../modules/quizgener/Genere4.js";
 export const QuizRoutes=express.Router() // creating routes
-QuizRoutes.get('/G1/:level',Genere1.Difficulty);  //using callback
-QuizRoutes.get('/G2/:level',Genere2.Difficulty);  //using callback
-QuizRoutes.get('/G3/:level',Genere3.Difficulty);  //using callback
-QuizRoutes.get('/G4/:level',Genere4.Difficulty);  //using callback
+QuizRoutes.get('/quiz/popculture/:level',Genere1.Difficulty);  //using callback
+QuizRoutes.get('/quiz/currentaffairs/:level',Genere2.Difficulty);  //using callback
+QuizRoutes.get('/quiz/science/:level',Genere3.Difficulty);  //using callback
+QuizRoutes.get('/quiz/technology/:level',Genere4.Difficulty);  //using callback
