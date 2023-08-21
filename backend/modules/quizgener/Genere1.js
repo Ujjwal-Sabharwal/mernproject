07fa2b690ac51fb45a1dd1bd9b09f35110b5f90a
Ujjwal-Sabharwal <142159680+Ujjwal-Sabharwal@users.noreply.github.com>
2023-08-21 16:10:52 +0530

    Update Genere1.js

--- backend/modules/quizgener/Genere1.js
+++ backend/modules/quizgener/Genere1.js
@@ -1,7 +1,7 @@
 export const Genere1={
     Difficulty(req,res){
         const diff=req.params.level //using parameters
-        console.log("genere1");
+        console.log("popculture");
         if(diff=='easy'){
             res.json({1:"q1",2:"q2",3:"q3",4:"q4",5:"q5",6:"q6",7:"q7",8:"q8",9:"q9",10:"q10"})
         }
