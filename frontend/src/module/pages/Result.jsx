bf08a919461ad28e134f5395ea51888ae90a7223
Ujjwal-Sabharwal <142159680+Ujjwal-Sabharwal@users.noreply.github.com>
2023-08-23 10:04:36 +0530

    Update Result.jsx

--- frontend/src/module/pages/Result.jsx
+++ frontend/src/module/pages/Result.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header } from '../../shared/component/Header'
 import { useParams } from 'react-router-dom'
 import { Container } from '@mui/material';
 import { ansoperation } from '../../shared/services/ansOperation';
+import { apiClient } from '../services/apicall';
 
-export const Result = () => {
-const data={1:['q1','o1*','o2*','o3*','o4*'],2:['q2','o1+','o2+','o3+','o4+'],3:['q3','o1-','o2-','o3-','o4-'],'ans':['o1*','o3+','o2-']}
+export const Result = (props) => {
+const param=useParams()
+const [data,setData]=useState({});
+const getdata= async (url)=>{
+  try{
+  const question = await apiClient.get(url);
+  console.log('data',question);
+  setData(question);
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+useEffect( ()=>{
+  const url=`http://localhost:1234/quiz/${param.genere}/${param.level}`
+  getdata(url);
+  
+ },[]);
+
+let result;
+if(data[1]){
 const answerarray=ansoperation.getans()
 let score=0
 let j=0
@@ -15,14 +35,19 @@ for(let i of data.ans){
   }
   j++
 }
-  const param=useParams();
-  const result=`${param.genere} ${param.level} Result=${score}`
+   result=`${param.genere} ${param.level} Result=${score}`}
+   else{
+    result='err'
+   }
   return (
+    <>
+    {data[1]?
     <div>
       <Header/>
         <Container maxWidth="sm">    
         <h1>{result}</h1>
         </Container>  
-    </div>
+    </div>:<div>loading</div>}
+    </>
   )
 }
